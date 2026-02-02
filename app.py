23a400b6acd0818a1dc862feeb74846b37ec186d
cabf05 <cabf05@gmail.com>
2026-02-02 09:27:06 -0500

    Update API URL for Tangerino integration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 st.title("Teste Tangerino /test")
 
-url = "https://apis.tangerino.com.br/punch/?adjustment=true&size=1&startDate=1"
+url = "https://employer.tangerino.com.br/test"
 
 headers = {
     "accept": "application/json;charset=UTF-8",
