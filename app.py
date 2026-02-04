d91b090a539515864bc93c7b61b7bab25406a705
cabf05 <cabf05@gmail.com>
2026-02-04 08:53:54 -0500

    Comment out User-Agent header in headers

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ headers = {
     "accept": "application/json;charset=UTF-8",
     "Authorization": st.secrets["TANGERINO_AUTH"]
     #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+                 # "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
 if st.button("Testar endpoint"):
